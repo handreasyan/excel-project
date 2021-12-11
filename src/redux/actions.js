a75b5906b9dab17718775bc460f787a1ae57afab
handreasyan <hakob.andreasyan@popupschool.com>
2021-12-11 19:39:11 +0400

    routing completed

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import {CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLE, CHANGE_TITLE} from '@/redux/types';
+import {CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLE, CHANGE_TITLE, UPDATE_DATE} from '@/redux/types';
 
 export function tableResize(data) {
   return { type: TABLE_RESIZE, data }
@@ -18,4 +18,7 @@ export function applyStyle(data) {
 
 export function changeTitle(data) {
   return { type: CHANGE_TITLE, data }
+}
+export function updateDate() {
+  return { type: UPDATE_DATE }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
@@ -63,6 +64,9 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: fileName('css')
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     })
   ],
   module: {
